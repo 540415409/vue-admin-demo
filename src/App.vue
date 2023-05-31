f33b44d46c7004e6b12c58c945e9af9b622e70af
大浪淘沙 <540415409@qq.com>
2023-05-31 16:27:17 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <router-view />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+
+html,
+body,
+#app,
+.el-container {
+  height: 100%;
+  width: 100%;
+  padding: 0 0;
+  margin: 0 0;
+  display: flex;
+  justify-content: center;
+  text-align: center;
+  color: #2c3e50;
+  font-size: 14px;
+  font-weight: 200;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+}
+</style>
---
+++ src/components/layout/index.vue
@@ -0,0 +1,132 @@
+<template>
+    <el-container>
+        <el-header>
+            <div class="header-body">
+                <div class="header-body-user">
+                    创作者平台
+                </div>
+                <div class="header-body-logout">
+                    <el-avatar icon="el-icon-user-solid"></el-avatar>
+                </div>
+            </div>
+        </el-header>
+        <el-main class="main-wrap">
+            <el-container>
+                <el-row class="main-body">
+                    <el-col :span="4">
+                        <el-aside width="250px" class="app-left">
+                            <el-menu active-text-color="#ffd04b">
+                                <el-submenu index="1">
+                                    <template slot="title">
+                                        <i class="el-icon-s-data"></i>
+                                        <span class="menu-title">内容管理</span>
+                                    </template>
+                                    <el-menu-item index="1-1">视频管理</el-menu-item>
+                                    <el-menu-item index="1-2">音乐管理</el-menu-item>
+                                </el-submenu>
+                                <el-submenu index="2">
+                                    <template slot="title">
+                                        <i class="el-icon-s-custom"></i>
+                                        <span class="menu-title">互动管理</span>
+                                    </template>
+                                    <el-menu-item index="11-1">关注管理</el-menu-item>
+                                    <el-menu-item index="11-2">粉丝管理</el-menu-item>
+                                    <el-menu-item index="11-3">评论管理</el-menu-item>
+                                </el-submenu>
+                                <el-submenu index="3">
+                                    <template slot="title">
+                                        <i class="el-icon-video-camera-solid"></i>
+                                        <span class="menu-title">视频数据</span>
+                                    </template>
+                                    <el-menu-item index="12-1">数据总览</el-menu-item>
+                                    <el-menu-item index="12-2">作品数据</el-menu-item>
+                                    <el-menu-item index="12-3">粉丝面向</el-menu-item>
+                                </el-submenu>
+                            </el-menu>
+                        </el-aside>
+                    </el-col>
+                    <el-col :span="20">
+                        <div class="main-context">
+                            <router-view />
+                        </div>
+                    </el-col>
+                </el-row>
+            </el-container>
+        </el-main>
+    </el-container>
+</template>
+
+<script>
+export default {
+    name: 'Index',
+    data() {
+        return {
+        }
+    }
+}
+</script>
+
+<style scoped>
+.el-main {
+    padding: 0 0;
+    margin: 0 0;
+    background-color: #d1d1d1;
+}
+
+.el-header,
+.el-main,
+.el-footer {
+    padding: 0 0;
+    margin: 0 0;
+    display: flex;
+    justify-content: center;
+    align-content: center;
+}
+
+.main-body,
+.header-body {
+    width: 70%;
+    height: 100%;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+}
+
+.header-body {
+    justify-content: space-between;
+    align-items: center;
+}
+
+.main-wrap {
+    padding-top: 15px;
+}
+
+.app-left,
+.main-context {
+    height: 100%;
+    background-color: #FFFFFF;
+}
+
+.el-menu {
+    margin: 10px 10px;
+    border: 0px;
+}
+
+/deep/ .el-submenu__title,
+.el-submenu .el-menu-item {
+    text-align: left !important;
+}
+
+.menu-title {
+    font-size: 16px;
+    font-weight: bolder;
+}
+
+.el-menu-item {
+    padding-left: 52px !important;
+}
+
+.el-submenu {
+    margin-right: 0px;
+}
+</style>
